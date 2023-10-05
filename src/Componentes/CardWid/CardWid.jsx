126a0e4c5f107b9c7f0a6a5488f6022345c50c0f
Maria Aparecida <maria.info108@gmail.com>
2023-10-05 12:48:18 -0300

    Animacao adicionadas nas sessoes

--- src/Componentes/CardWid/CardWid.jsx
+++ src/Componentes/CardWid/CardWid.jsx
@@ -1,13 +1,16 @@
 import styles from "./CardWid.module.css";
+import { Fade } from 'react-reveal';
 
 export default function CardWid({numero}) {
     return (
 
         <div className={styles.container}>
             <div className={styles.caixa}>
+                <Fade top>
                 <div className={styles.caixinha}>
                    {numero}
                 </div>
+                </Fade>
                 <div className={styles.desc}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mi nulla, vulputate quis interdum ac, venenatis
                 </div>
--- src/Componentes/Sessoes/Sessao6.jsx
+++ src/Componentes/Sessoes/Sessao6.jsx
@@ -5,9 +5,10 @@ import { Title } from '../Title.style';
 import { Description } from '../description.styles';
 import styled from 'styled-components';
 import { Botton } from '../Botton.style';
+import { Fade } from 'react-reveal';
 export default function Sessao6() {
-   
-   const Line = styled.div`
+
+    const Line = styled.div`
       
     display: flex;
     flex-direction: row;
@@ -21,16 +22,18 @@ export default function Sessao6() {
 
         <Wrapper cor={'#35f5a1'}>
             <Container>
-                <Line>
-                    <div>
-                        <Slogan cor={'#000'}>SAFE / SECURE / PRIVATE</Slogan>
-                        <Title cor={'#000'}>The future of finance is DeFi</Title>
-                        <Description tamanho={'720px'}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec augue semper, malesuada tellus quis, ultricies augue. Cras tincidunt non tellus quis euismod. Vivamus aliquet tortor pharetra erat pellentesque, non consequat nibh sagittis. In sed.</Description>
-                    </div>
-                    <div>
-                        <Botton color={'#fff'} background={'#000'}>GET INVOLVED ➔ </Botton>
-                    </div>
-                </Line>
+                <Fade duration={2000}>
+                    <Line>
+                        <div>
+                            <Slogan cor={'#000'}>SAFE / SECURE / PRIVATE</Slogan>
+                            <Title cor={'#000'}>The future of finance is DeFi</Title>
+                            <Description tamanho={'720px'}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec augue semper, malesuada tellus quis, ultricies augue. Cras tincidunt non tellus quis euismod. Vivamus aliquet tortor pharetra erat pellentesque, non consequat nibh sagittis. In sed.</Description>
+                        </div>
+                        <div>
+                            <Botton color={'#fff'} background={'#000'}>GET INVOLVED ➔ </Botton>
+                        </div>
+                    </Line>
+                </Fade>
             </Container>
 
         </Wrapper>
